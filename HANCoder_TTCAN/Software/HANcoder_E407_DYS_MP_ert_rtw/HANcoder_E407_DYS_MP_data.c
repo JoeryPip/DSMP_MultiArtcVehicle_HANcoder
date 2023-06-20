8ec4e1e529a53adecbc8b1fe995f19b0eca0f97a
JoeryPip <137149453+JoeryPip@users.noreply.github.com>
2023-06-20 09:09:51 +0200

    Add files via upload

---
+++ HANCoder_TTCAN/Software/HANcoder_E407_DYS_MP_ert_rtw/HANcoder_E407_DYS_MP_data.c
@@ -0,0 +1,45 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * File: HANcoder_E407_DYS_MP_data.c
+ *
+ * Code generated for Simulink model 'HANcoder_E407_DYS_MP'.
+ *
+ * Model version                  : 1.136
+ * Simulink Coder version         : 9.0 (R2018b) 24-May-2018
+ * C/C++ source code generated on : Tue Jun 20 01:51:50 2023
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "HANcoder_E407_DYS_MP.h"
+#include "HANcoder_E407_DYS_MP_private.h"
+
+/* Invariant block signals (default storage) */
+const ConstBlockIO rtConstB = {
+  /* Start of '<S5>/4.3 TTA System' */
+  {
+    26.0,                              /* '<S285>/Sum' */
+    26.0,                              /* '<S285>/Sum1' */
+    10.0,                              /* '<S214>/Divide10' */
+    3.3333333333333335,                /* '<S214>/Gain' */
+    10.0,                              /* '<S214>/Divide2' */
+    1.0,                               /* '<S126>/Add1' */
+    0U,                                /* '<S135>/Cast' */
+    0U,                                /* '<S126>/Cast' */
+    0U,                                /* '<S135>/Cast1' */
+    0                                  /* '<S142>/GreaterThanOrEqual24' */
+  }
+  /* End of '<S5>/4.3 TTA System' */
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
