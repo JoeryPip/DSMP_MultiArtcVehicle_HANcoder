3e18dc0decc4ab8ef30471562ef069edc7048cfd
JoeryPip <137149453+JoeryPip@users.noreply.github.com>
2023-06-20 08:54:48 +0200

    Delete HANcoder_E407_DYS_MP_ert_rtw directory

--- HANcoder_E407_DYS_MP_ert_rtw/SYS_config.h
+++
@@ -1,6 +0,0 @@
-/* add define with Application control task timing */
-#define APP_CTRL_TASK_TIMING           10
-#define APP_CTRL_TASK_TIMING_US        10000
-#define kXcpStationIdString            "HANcoder_E407_DYS_MP_softwareID_v1_200623_015142"
-#define kXcpStationIdLength            48
-#define NUMFILTEREDCHANNELS            0
